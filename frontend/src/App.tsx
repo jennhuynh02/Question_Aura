e4aad3ac65e38103e16e04fb954d28c5e16ff490
jennhuynh02 <jennhuynh637@gmail.com>
2024-06-17 00:24:50 -0700

    Setup frontend skeleton with react and tailwind

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+// import SampleComponent from "./components/SampleComponent";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hello
+        {/* <SampleComponent /> */}
+      </header>
+    </div>
+  );
+};
+
+export default App;
